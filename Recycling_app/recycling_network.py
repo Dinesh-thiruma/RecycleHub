39ed03eb0501502887a06d9ca5d0d1f103afd43c
adithyachan <21148381+adithyachan@users.noreply.github.com>
2020-07-20 15:59:58 -0700

    Updated recycling-network
    Got within 80% accuracy for trash and substantially better for other classifiers. Need to look for more data to feed in for trash

--- Recycling_app/recycling_network.py
+++ Recycling_app/recycling_network.py
@@ -18,10 +18,10 @@ for param in recyclingNet.features.parameters():
     param.requires_grad = False
 
 
-
 data_dir = './trash_data/Garbage classification/Garbage classification'
 
-transform = transforms.Compose([transforms.Resize(225), transforms.CenterCrop(224), transforms.ToTensor(),                                                    transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])])
+transform = transforms.Compose([transforms.Resize(225), transforms.ToTensor(), transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])])
+
 
 data = datasets.ImageFolder(data_dir, transform=transform)
 
@@ -32,29 +32,27 @@ if use_cuda:
 else:
     criterion = nn.CrossEntropyLoss()
 
-optimizer = torch.optim.SGD(recyclingNet.parameters(), lr=.005, momentum=.9)
+optimizer = torch.optim.SGD(recyclingNet.parameters(), lr=.003, momentum=0.9)
 
-def train(epochs, loader, model, optimizer, criterion, use_cuda, save_path):
+def train(epochs, loader, model, optimize, criter, use_cuda, save_path):
 
   min_loss = np.Inf
 
   for ii in range(1, epochs+1):
-
     current_loss = 0
 
     for batch_idx, (data, target) in enumerate(data_loader):
-      
       if use_cuda:
-        data, target = data.cuda(), target.cuda()
-        
-      optimizer.zero_grad()
+          data, target = data.cuda(), target.cuda()
+      optimize.zero_grad()
       output = model(data)
-      loss = criterion(output, target)
+      loss = criter(output, target)
       loss.backward()
-      optimizer.step()
+      optimize.step()
 
       current_loss = current_loss + ((1 / (batch_idx + 1)) * (loss.data - current_loss))
       print("\nEpoch: " + str(ii) + " Loss: " + str(float(loss)))
+
 
 
       if min_loss > current_loss:
@@ -63,4 +61,4 @@ def train(epochs, loader, model, optimizer, criterion, use_cuda, save_path):
         torch.save(model.state_dict(), save_path)
 
 
-train(2, data_loader, recyclingNet, optimizer, criterion, use_cuda, "./recycling_net.pt")
+train(10, data_loader, recyclingNet, optimizer, criterion, use_cuda, "./recycling_net.pt")
